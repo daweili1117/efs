beb9368efb265658821284b35afcf2a2f031966f
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2021-02-21 20:17:21 -0600

    change and forget password

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -27,6 +28,11 @@ urlpatterns = [
     path('customer/<int:pk>/portfolio/', views.portfolio, name='portfolio'),
 
     url(r'^customers_json/', views.CustomerList.as_view()),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
